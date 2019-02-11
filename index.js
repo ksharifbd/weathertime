e11594d3c076a59bdfcac8c8434f8753aea723ee
ksharifbd <ksharifbd@gmail.com>
2019-02-11 11:58:22 +0600

    restructures index.js

--- index.js
+++ index.js
@@ -3,44 +3,50 @@ const commander = require('commander');
 const time = require('./time');
 const weather = require('./weather');
 
-const commands = ['--wak', '--tak'];
+let apiKey = {
+    commands: ['wak', 'tak'],
+    wak: ['weather'],
+    tak: ['time'],
+};
 
 commander
     .version('0.1.0', '-v, --version')
-    .option(commands[0], 'provide weather api key')
-    .option(commands[1], 'provide time api key')
+    .option(`--${apiKey.commands[0]}`, 'provide weather api key')
+    .option(`--${apiKey.commands[1]}`, 'provide time api key')
     .parse(process.argv);
 
 const args = process.argv.slice(2);
-const removableValuesFromArgs = [...commands];
 
-let weatherApiKey;
-let timeApiKey;
+const removableValuesFromArgs = apiKey.commands.map(command => `--${command}`);
 
-if(commander.wak) {
-    weatherApiKey = args[args.indexOf('--wak') + 1];
-    removableValuesFromArgs.push(weatherApiKey);
-} else {
-    console.log('please provide weather api key using --wak');
+const storeApiKey = command => {
+    if(commander[command]) {
+        apiKey[command].push(args[args.indexOf(`--${command}`) + 1]);
+        removableValuesFromArgs.push(apiKey[command][1]);
+    }
+};
+
+const showMissingArgMessage = command => {
+    if(!commander[command]) {
+        console.log(`Please provide api key for ${apiKey[command][0]} using --${command}`);
+    }
 }
 
-if(commander.tak) {
-    timeApiKey = args[args.indexOf('--tak') + 1];
-    removableValuesFromArgs.push(timeApiKey);
-} else {
-    console.log('please provide time api key using --tak');
-}
+apiKey.commands.forEach(command => {
+    storeApiKey(command);
+    showMissingArgMessage(command);
+});
 
 args
     .filter(arg => !removableValuesFromArgs.includes(arg))
     .forEach(async argument => {
 
     try {
-        const weatherData = await weather(weatherApiKey, argument);
+        const weatherData = await weather(apiKey.wak[1], argument);
 
         const {lat, lon} = weatherData.coord;
     
-        const timeData = await time(timeApiKey, lat, lon);
+        const timeData = await time(apiKey.tak[1], lat, lon);
     
         const output = {
             name: weatherData.name,
